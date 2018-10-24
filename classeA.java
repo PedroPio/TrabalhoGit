e89b504562f81e39025ffc119b08e97825d9b6ff
PedroPio <pedropio935@gmail.com>
2018-10-24 16:13:24 -0300

    criando a classeA
    Classe criada com atributos float a e float b e seus métodos get e set

---
+++ classeA.java
@@ -0,0 +1,19 @@
+public class ClasseA{
+    private float a;
+    private float b;
+
+    public float getA(){
+        return a;
+    }
+    public float getB(){
+        return b;
+    }
+    public void setA(float a){
+        a = a -1;
+
+    }
+    public void setB(float b){
+        b = b -1;
+
+    }
+}
