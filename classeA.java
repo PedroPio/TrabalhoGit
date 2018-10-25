7615f43b0befa310ce8bae2db27214f642cc7810
PedroPio <pedropio935@gmail.com>
2018-10-24 21:43:18 -0300

    inserido novo atributo aprinc4

--- classeA.java
+++ classeA.java
@@ -2,6 +2,7 @@ public class ClasseA{
     private int a1;
     private float a2;
     private String a3;
+    private String aprinc4;
 
     public int getA1(){
         return a1;
@@ -20,10 +21,17 @@ public class ClasseA{
     public String getA3(){
         return a3;
     }
-    public void setA3(String A3){
+    public void setA3(String a3){
         a3 = "";
     }
     public float soma(int a1, float a2){
         return a1 + a2 ;
     }
+    public String getAprinc4(){
+        return a3;
+    }
+    public void setAprinc4(String aprinc4){
+        a3 = "";
+    }
+    
 }
