d38f6b2a3c996788eb1d68f0679c30f92904a667
PedroPio <pedropio935@gmail.com>
2018-10-24 21:35:19 -0300

    criado atributo aramo4 com seus metodos get e set ramo1

--- classeA.java
+++ classeA.java
@@ -2,6 +2,7 @@ public class ClasseA{
     private int a1;
     private float a2;
     private String a3;
+    private String aramo4;
 
     public int getA1(){
         return a1;
@@ -20,7 +21,13 @@ public class ClasseA{
     public String getA3(){
         return a3;
     }
-    public void setA3(String A3){
+    public void setA3(String a3){
+        a3 = "";
+    }
+    public String getAramo4(){
+        return a3;
+    }
+    public void setAramo4(String aramo4){
         a3 = "";
     }
     public float soma(int a1, float a2){
